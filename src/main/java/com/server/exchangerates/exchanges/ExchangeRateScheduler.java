617a3e9c6f12438dc6f89fab571852971bb48f8a
Surith <surithcodes204@gmail.com>
2024-07-09 12:26:40 +0530

    caching being implemented

---
+++ src/main/java/com/server/exchangerates/exchanges/ExchangeRateScheduler.java
@@ -0,0 +1,19 @@
+package com.server.exchangerates.exchanges;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExchangeRateScheduler {
+    private final ExchangeService exchangeService;
+
+    public ExchangeRateScheduler(ExchangeService exchangeService){
+        this.exchangeService = exchangeService;
+    }
+
+    //setting up the cron operation for everyday at 12:00 AM
+    @Scheduled(cron = "0 0 0 * * *")
+    public void updateExchangeRates(){
+        exchangeService.fetchAndCacheLatestRates();
+    }
+}
--- src/main/java/com/server/exchangerates/exchanges/ExchangeService.java
+++ src/main/java/com/server/exchangerates/exchanges/ExchangeService.java
@@ -1,34 +1,64 @@
 package com.server.exchangerates.exchanges;
 
+import java.time.Duration;
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import reactor.core.publisher.Mono;
 
-
 @Service
 public class ExchangeService {
     private final WebClient webClient;
+    private final RedisTemplate<String , Object> redisTemplate;
     private final String apikey;
     private final String defaultCurrency;
 
-    public ExchangeService(WebClient.Builder webClientBuilder,
-                        @Value("${exchange.api.url}") String baseUrl,
+    public ExchangeService(WebClient webClient,
+                        RedisTemplate<String, Object> redisTemplate,
                         @Value("${exchange.api.key}") String apikey,
                         @Value("${exchange.api.defaultcurrency}") String defaultCurrency){
-                            this.webClient = webClientBuilder.baseUrl(baseUrl).build();
+                            this.webClient = webClient;
+                            this.redisTemplate = redisTemplate;
                             this.apikey = apikey;
                             this.defaultCurrency = defaultCurrency;
-                        }
+    }
+
+    public void fetchAndCacheLatestRates(){
+        ExchangeResponse rates = webClient.get()
+                .uri("/{apikey}/latest/{defaultCurrency}", apikey, defaultCurrency)
+                .retrieve()
+                .bodyToMono(ExchangeResponse.class)
+                .block();
+        
+        String cacheKey = "exchange_rates_" + LocalDate.now();
+        redisTemplate.opsForValue().set(cacheKey , rates , Duration.ofDays(2));
+    }
 
     public Mono<ExchangeResponse> getRates(){
+        String cacheKey = "exchange_rates_" + LocalDate.now();
+        ExchangeResponse cachedRates = (ExchangeResponse) redisTemplate.opsForValue().get(cacheKey);
+
+        if(cachedRates != null){
+            return Mono.just(cachedRates);
+        }else{
+            return fetchAndCacheLatestRates2();
+        }
+    }
+
+    public Mono<ExchangeResponse> fetchAndCacheLatestRates2(){
         return webClient.get()
                 .uri("/{apikey}/latest/{defaultCurrency}", apikey, defaultCurrency)
                 .retrieve()
-                .bodyToMono(ExchangeResponse.class);
+                .bodyToMono(ExchangeResponse.class)
+                .doOnNext(rates -> {
+                    String cacheKey = "exchange_rates_" + LocalDate.now();
+                    redisTemplate.opsForValue().set(cacheKey, rates, Duration.ofDays(2));
+                });
     }
-
     public Mono<ConversionResponse> convert(String from , String to , Double amount){
         return getRates()
                 .map(response -> {
@@ -37,8 +67,5 @@ public class ExchangeService {
                     Double convertedAmount = (amount / fromRate) * toRate;
                     return new ConversionResponse(from, to, amount, convertedAmount);
                 });
-    }
-                        
-                                          
-
+    }                                                      
 }
